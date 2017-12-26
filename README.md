ebfdd5cc4ca0fb579beec0ed9b6ab47be0f4cfdd
ShallyBanh <banh.shally@gmail.com>
2017-12-25 18:23:13 -0700

    added completing/removing an completed event from our todo list

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # TodoList
-A simple todo list web app using react.js and shopify's polaris library https://polaris.shopify.com/ 
+A simple todo list web app using react.js and shopify's polaris library https://polaris.shopify.com/
+
+
+##Inspiration
+
+Kind of wanted to learn react and i needed to find a better todo list app to keep myself organized so i figure why not kill two birds with one stone and build one myself
--- todolist-app/src/TodoList.js
+++ todolist-app/src/TodoList.js
@@ -10,6 +10,7 @@ class TodoList extends Component {
     };
 
     this.addItem = this.addItem.bind(this);
+    this.deleteItem = this.deleteItem.bind(this);
   }
 
   addItem(e){
@@ -34,6 +35,17 @@ class TodoList extends Component {
 
   }
 
+  deleteItem(key) {
+    var filteredItems = this.state.items.filter(function (item) {
+      return (item.key !== key);
+    });
+
+    this.setState({
+      items: filteredItems
+    });
+  }
+
+
   render() {
     return (
       <div className="todoListMain">
@@ -44,7 +56,7 @@ class TodoList extends Component {
             <button type="submit">add</button>
           </form>
         </div>
-        <TodoItems entries={this.state.items}/>
+        <TodoItems entries={this.state.items} delete={this.deleteItem}/>
       </div>
     );
   }
